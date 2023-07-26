1cd83771718ba6edc08c66f1f1ef1174f5da121e
htloc0610 <12a1huynhtanloc@gmail.com>
2023-07-26 22:53:15 +0700

    M2D6

---
+++ M2D6.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+int calculate(int n) {
+    int count = 0;
+
+    while (n != 1) {
+        if (n % 6 == 0) {
+            n /= 6;
+        }
+        else if (n % 3 == 0) {
+            n *= 2;
+        }
+        else {
+            // Không thể thực hiện phép tính
+            return -1;
+        }
+        count++;
+    }
+
+    return count;
+}
+
+int main() {
+    int n = 0;
+    cin >> n;
+    while (n != 0) {
+        int m = 0;
+        cin >> m;
+        cout << calculate(m) << endl;
+        --n;
+    }
+    return 0;
+}
